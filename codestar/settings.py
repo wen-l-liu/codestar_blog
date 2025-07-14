b2f23f5f294a110d99b9c29c96b770987fade647
wen-l-liu <w.l.liu180@gmail.com>
2025-07-14 17:37:54 +0100

    heroky update key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uh=#vu$=g$p&6!ur9ur1h!z9@vps0y$p=vvqh(glw(p#znspf!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
