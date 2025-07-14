1ef24247bcf059548bc43bbe014d6e6adbf3f894
wen-l-liu <w.l.liu180@gmail.com>
2025-07-14 20:28:46 +0100

    add excerot to post model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,4 @@ class Post(models.Model):
     # This is the blog article content.
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
