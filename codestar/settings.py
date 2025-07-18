ec5e2f7b081647ef68b43e98536e8976626932eb
wen-l-liu <w.l.liu180@gmail.com>
2025-07-18 16:55:51 +0100

    add views for post

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+if os.path.isfile('env.py'):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
