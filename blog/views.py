a529d02c7ef4d03ad159f30c3586134747cd572a
wen-l-liu <w.l.liu180@gmail.com>
2025-07-19 22:31:02 +0100

    update pagination to show 9 posts per page and add previous page link in index template

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,4 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 9
