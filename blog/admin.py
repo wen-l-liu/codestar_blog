1547bc92462051865df4038dede5b50de4b208f9
wen-l-liu <w.l.liu180@gmail.com>
2025-07-19 21:58:32 +0100

    add templates for base and index

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
