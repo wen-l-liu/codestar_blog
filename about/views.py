fc26ae53df59a7526639078897bfe6601576f051
wen-l-liu <w.l.liu180@gmail.com>
2025-07-29 21:53:23 +0100

    add comment editing functionality

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -9,9 +10,27 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
+    
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                (
+                    "Collaboration request received! "
+                    "I endeavor to respond within 2 working days."
+                )
+            )
+        else:
+            messages.add_message(
+                request, messages.ERROR,
+                'There was an error with your submission'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
-
+    
     return render(
         request,
         "about/about.html",
